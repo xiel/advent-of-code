090655feaff0af3397e86c74c7c8bfd4af3b055c
Felix Leupold <felix.leupold-extern@deutschebahn.com>
2020-12-04 09:12:01 +0100

    day 02 - part II

--- src/Day 02/01-Password-Policy.test.ts
+++ src/Day 02/01-Password-Policy.test.ts
@@ -1,10 +1,11 @@
 import {
   filterValidPasswords,
   isValidPasswordValidSledRentalPlace,
+  isValidPasswordValidTobogganCorporatePolicy,
 } from "./filterValidPasswords";
 import { passwordDBEntries } from "./input";
 
-describe("Password Philosophy", () => {
+describe("Password Philosophy - sled rental place", () => {
   it("should work on provided example data", () => {
     const entries = ["1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc"];
     expect(
@@ -19,5 +20,24 @@ describe("Password Philosophy", () => {
         isValidPasswordValidSledRentalPlace
       ).length
     ).toMatchInlineSnapshot(`456`);
+  });
+});
+
+describe("Password Philosophy - Toboggan Corporate", () => {
+  it("should work on provided example data", () => {
+    const entries = ["1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc"];
+    expect(
+      filterValidPasswords(entries, isValidPasswordValidTobogganCorporatePolicy)
+        .length
+    ).toBe(1);
+  });
+
+  it("should work on puzzle input", function () {
+    expect(
+      filterValidPasswords(
+        passwordDBEntries,
+        isValidPasswordValidTobogganCorporatePolicy
+      ).length
+    ).toMatchInlineSnapshot(`308`);
   });
 });
--- src/Day 02/filterValidPasswords.ts
+++ src/Day 02/filterValidPasswords.ts
@@ -23,6 +23,25 @@ export function isValidPasswordValidSledRentalPlace(
   return foundOccurrences >= minOccurrence && foundOccurrences <= maxOccurrence;
 }
 
+export function isValidPasswordValidTobogganCorporatePolicy(
+  passwordEntry: PasswordEntry
+): boolean {
+  const [occurrenceRangeString, letter, password] = passwordEntry
+    .replace(":", "")
+    .split(" ");
+
+  // get index, string is not zero index based, so we subtract 1 to make it so
+  const [pos1, pos2] = occurrenceRangeString
+    .split("-")
+    .map((numberStr) => parseInt(numberStr) - 1);
+
+  const posOneMatches = password[pos1] === letter;
+  const posTwoMatches = password[pos2] === letter;
+
+  // only one must be true, not both!
+  return (posOneMatches || posTwoMatches) && posOneMatches !== posTwoMatches;
+}
+
 export function filterValidPasswords(
   entries: PasswordEntry[],
   filterFn: (password: PasswordEntry) => boolean
