9fedf515b60af299a25d044ff5d8b571f74b55ef
Felix Leupold <felix.leupold-extern@deutschebahn.com>
2020-12-04 08:56:45 +0100

    day 02 - part I

--- src/Day 02/01-Password-Policy.test.ts
+++ src/Day 02/01-Password-Policy.test.ts
@@ -1,8 +1,15 @@
 import { filterValidPasswords } from "./filterValidPasswords";
+import { passwordDBEntries } from "./input";
 
 describe("Password Philosophy", () => {
   it("should work on provided example data", () => {
     const entries = ["1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc"];
     expect(filterValidPasswords(entries).length).toBe(2);
+  });
+
+  it("should work on puzzle input", function () {
+    expect(
+      filterValidPasswords(passwordDBEntries).length
+    ).toMatchInlineSnapshot(`456`);
   });
 });
--- src/Day 02/filterValidPasswords.ts
+++ src/Day 02/filterValidPasswords.ts
@@ -1,8 +1,26 @@
 // can be expected to be in format: "1-3 b: cdefg"
 type PasswordEntry = string;
 
-function isPasswordValid() {}
+function isPasswordValid(passwordEntry: PasswordEntry) {
+  const [occurrenceRangeString, letter, password] = passwordEntry
+    .replace(":", "")
+    .split(" ");
+
+  const [minOccurrence, maxOccurrence] = occurrenceRangeString
+    .split("-")
+    .map((numberStr) => parseInt(numberStr));
+
+  let foundOccurrences = 0;
+
+  for (const currentLetter of password) {
+    if (currentLetter === letter) {
+      foundOccurrences += 1;
+    }
+  }
+
+  return foundOccurrences >= minOccurrence && foundOccurrences <= maxOccurrence;
+}
 
 export function filterValidPasswords(entries: PasswordEntry[]) {
-  return [];
+  return entries.filter(isPasswordValid);
 }
