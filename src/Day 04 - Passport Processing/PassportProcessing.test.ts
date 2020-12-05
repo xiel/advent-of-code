fdbb09b1c62fc8be58c92b18fb3b2c89be5ebcca
Felix Leupold <felix@xiel.de>
2020-12-05 10:12:55 +0100

    Day 4 - part II

--- src/Day 04 - Passport Processing/PassportProcessing.test.ts
+++ src/Day 04 - Passport Processing/PassportProcessing.test.ts
@@ -5,7 +5,7 @@ function readFile(filename: string) {
   return fs.readFileSync(`${__dirname}/${filename}`, "utf-8");
 }
 
-describe("Passport Processing", () => {
+describe("Passport Processing - non strict", () => {
   it("should count valid passports with cid optional (example)", () => {
     const passportBatchFileText = readFile("example.txt");
     expect(countValidPassports(passportBatchFileText)).toBe(2);
@@ -16,5 +16,22 @@ describe("Passport Processing", () => {
     expect(countValidPassports(passportBatchFileText)).toMatchInlineSnapshot(
       `228`
     );
+  });
+});
+
+describe("Passport Processing - strict", () => {
+  it("should count strictly valid passports with cid optional (examples)", () => {
+    expect(
+      countValidPassports(readFile("exampleStrictInvalid.txt"), true)
+    ).toBe(0);
+    expect(countValidPassports(readFile("exampleStrictValid.txt"), true)).toBe(
+      4
+    );
+  });
+
+  it("should count strictly valid passports with cid optional (input)", () => {
+    expect(
+      countValidPassports(readFile("input.txt"), true)
+    ).toMatchInlineSnapshot(`175`);
   });
 });
--- src/Day 04 - Passport Processing/passportProcessing.ts
+++ src/Day 04 - Passport Processing/passportProcessing.ts
@@ -30,30 +30,88 @@ function passportsAsEntries(passportStrings: string[]) {
   return passportStrings.map(transformToPassportEntry);
 }
 
+const requiredPassportFields: PassportFieldName[] = [
+  "byr",
+  "iyr",
+  "eyr",
+  "hgt",
+  "hcl",
+  "ecl",
+  "pid",
+];
+
+const validators = {
+  //byr (Birth Year) - four digits; at least 1920 and at most 2002.
+  byr: (value: string) => {
+    const valueAsNumber = Number(value);
+    return valueAsNumber >= 1920 && valueAsNumber <= 2002;
+  },
+  //iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+  iyr: (value: string) => {
+    const valueAsNumber = Number(value);
+    return valueAsNumber >= 2010 && valueAsNumber <= 2020;
+  },
+  //eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+  eyr: (value: string) => {
+    const valueAsNumber = Number(value);
+    return valueAsNumber >= 2020 && valueAsNumber <= 2030;
+  },
+  //hgt (Height) - a number followed by either cm or in:
+  //If cm, the number must be at least 150 and at most 193.
+  //If in, the number must be at least 59 and at most 76.
+  hgt: (value: string) => {
+    const unit = value.endsWith("cm") ? "cm" : "in";
+    const [unitlessValue] = value.split(unit);
+    const valueAsNumber = Number(unitlessValue);
+
+    if (unit === "cm") {
+      return valueAsNumber >= 150 && valueAsNumber <= 193;
+    } else if (unit === "in") {
+      return valueAsNumber >= 59 && valueAsNumber <= 76;
+    }
+  },
+  //hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+  hcl: (value: string) => {
+    return !!value.match(/#[0-9a-f]{6}$/);
+  },
+
+  //ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+  ecl: (value: string) => {
+    return ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].includes(value);
+  },
+
+  //pid (Passport ID) - a nine-digit number, including leading zeroes.
+  pid: (value: string) => {
+    return !!value.match(/^\d{9}$/);
+  },
+  //cid (Country ID) - ignored, missing or not.
+  cid: () => true,
+};
+
 function isPassportValid(
   passportEntry: Partial<PassportEntry>,
-  requiredPassportFields: PassportFieldName[] = [
-    "byr",
-    "iyr",
-    "eyr",
-    "hgt",
-    "hcl",
-    "ecl",
-    "pid",
-  ]
+  strict: boolean
 ) {
-  return requiredPassportFields.every(
-    (fieldName) => !!passportEntry[fieldName]
-  );
+  return requiredPassportFields.every((fieldName) => {
+    const value = passportEntry[fieldName];
+
+    if (!value) return false;
+    if (!strict) return true;
+
+    return validators[fieldName](value);
+  });
 }
 
-export function countValidPassports(passportBatchFileText: string) {
+export function countValidPassports(
+  passportBatchFileText: string,
+  strict = false
+): number {
   const passportEntries = passportsAsEntries(
     parseSeparatePassportStrings(passportBatchFileText)
   );
 
   const validPassports = passportEntries.filter((passportEntry) =>
-    isPassportValid(passportEntry)
+    isPassportValid(passportEntry, strict)
   );
 
   return validPassports.length;
