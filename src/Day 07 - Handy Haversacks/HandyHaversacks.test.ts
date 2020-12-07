55441e2484e360f8a3eba3c97a76e7c08b41fc26
Felix Leupold <felix@xiel.de>
2020-12-07 11:28:03 +0100

    2020 - Day 07 - Part II

--- src/Day 07 - Handy Haversacks/HandyHaversacks.test.ts
+++ src/Day 07 - Handy Haversacks/HandyHaversacks.test.ts
@@ -1,28 +1,58 @@
 import { readFileIntoLines } from "../utils/readFile";
 
 describe("Day 7: Handy Haversacks", () => {
-  test("Example - How many bag colors can eventually contain at least one shiny gold bag?", () => {
-    const example = readFileIntoLines(`${__dirname}/fixtures/example.txt`);
-    expect(countBagsThatCanContainOneShinyGoldBag(example)).toEqual(4);
-  });
+  describe("How many bag colors can eventually contain at least one shiny gold bag?", () => {
+    test("Example", () => {
+      const example = readFileIntoLines(`${__dirname}/fixtures/example.txt`);
+      expect(countBagsThatCanContainOneShinyGoldBag(example)).toEqual(4);
+    });
 
-  test("Input - How many bag colors can eventually contain at least one shiny gold bag?", () => {
-    const example = readFileIntoLines(`${__dirname}/fixtures/input.txt`);
-    expect(
-      countBagsThatCanContainOneShinyGoldBag(example)
-    ).toMatchInlineSnapshot(`316`);
+    test("Input", () => {
+      const example = readFileIntoLines(`${__dirname}/fixtures/input.txt`);
+      expect(
+        countBagsThatCanContainOneShinyGoldBag(example)
+      ).toMatchInlineSnapshot(`316`);
+    });
+  });
+});
+
+describe("Day 7: Handy Haversacks - Part II", () => {
+  describe("How many individual bags are required inside your single shiny gold bag?", () => {
+    test("Example", () => {
+      const example = readFileIntoLines(
+        `${__dirname}/fixtures/examplePart2.txt`
+      );
+      expect(countBagsInShinyGoldBag(example)).toEqual(126);
+    });
+
+    test("Input", () => {
+      const example = readFileIntoLines(`${__dirname}/fixtures/input.txt`);
+      expect(countBagsInShinyGoldBag(example)).toMatchInlineSnapshot(`11310`);
+    });
   });
 });
 
 // rule example: vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
 type Rules = string[];
+type Color = string;
+type Contain = {
+  color: string;
+  count: number;
+};
+type MustContain = Contain[];
+type ParsedBagRules = Map<Color, () => MustContain>;
+
+function countBagsInShinyGoldBag(rules: Rules) {
+  const bagRules = parseBagRules(rules);
+  const shinyGoldContains = bagRules.get("shiny gold")?.() || [];
+  return shinyGoldContains.reduce((acc, contain) => acc + contain.count, 0);
+}
 
 function countBagsThatCanContainOneShinyGoldBag(rules: Rules) {
   const bagRules = parseBagRules(rules);
-  const shinyGoldIncludedIn = Array.from(
-    bagRules.entries()
-  ).filter(([_, canContain]) => canContain().includes("shiny gold"));
-
+  const shinyGoldIncludedIn = Array.from(bagRules).filter(([_, canContain]) =>
+    canContain().find((contain) => contain.color === "shiny gold")
+  );
   return shinyGoldIncludedIn.length;
 }
 
@@ -32,52 +62,51 @@ function countBagsThatCanContainOneShinyGoldBag(rules: Rules) {
 function parseRule(ruleStr: string) {
   const [bagDescription, bagContainmentStr] = ruleStr.split("contain");
   const color = bagDescription.replace("bags", "").trim();
-  const canContain = bagContainmentStr.includes("no other bags")
+  const mustContain: Contain[] = bagContainmentStr.includes("no other bags")
     ? []
     : bagContainmentStr.split(",").map((str) => {
         const [countStr, colorA, colorB] = str.trim().split(" "); // example: 99 dotted black bags
         return {
-          count: Number(countStr),
           color: `${colorA} ${colorB}`,
+          count: Number(countStr),
         };
       });
 
   return {
     color,
-    canContain,
+    mustContain,
   };
 }
-
-type Color = string;
-type CanContain = string[];
-type ParsedBagRules = Map<Color, () => CanContain>;
 
 function parseBagRules(rulesAsStrings: Rules): ParsedBagRules {
   const bagRules: ParsedBagRules = new Map();
 
   for (const ruleStr of rulesAsStrings) {
     const currentRule = parseRule(ruleStr);
-    const containsColors = currentRule.canContain.map((c) => c.color);
 
-    let resolvedContains!: CanContain;
-
-    const getContains = (): CanContain => {
+    // resolve contains dynamically when read for the first time
+    // cache result for faster subsequent reads
+    let resolvedContains!: MustContain;
+    const getContains = (): MustContain => {
       if (resolvedContains) return resolvedContains;
 
-      resolvedContains = containsColors.reduce<CanContain>((acc, color) => {
-        acc.push(color);
+      return (resolvedContains = currentRule.mustContain.reduce<MustContain>(
+        (acc, contain) => {
+          acc.push(contain);
 
-        const getSubContainment = bagRules.get(color);
-        if (getSubContainment) {
-          getSubContainment().forEach((subContainmentColor) =>
-            acc.push(subContainmentColor)
+          const subContains = bagRules.get(contain.color)?.() || [];
+
+          subContains.forEach((subContain) =>
+            acc.push({
+              color: subContain.color,
+              count: subContain.count * contain.count,
+            })
           );
-        }
 
-        return acc;
-      }, []);
-
-      return resolvedContains;
+          return acc;
+        },
+        []
+      ));
     };
 
     bagRules.set(currentRule.color, getContains);
