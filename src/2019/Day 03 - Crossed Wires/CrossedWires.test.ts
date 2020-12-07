1e1055a2dc2530396faf55805ba02c39b049af44
Felix Leupold <felix@xiel.de>
2020-12-07 21:08:56 +0100

    2019 - Day 3 - part I - good performance

--- src/2019/Day 03 - Crossed Wires/CrossedWires.test.ts
+++ src/2019/Day 03 - Crossed Wires/CrossedWires.test.ts
@@ -2,10 +2,6 @@ import { readFileIntoLines } from "../../utils/readFile"
 
 type WirePathStr = string
 type XY = { x: number; y: number }
-type ClosestIntersection = { x: number; y: number; distance: number }
-type XYPosWithPossibleIntersection = XY & {
-  closestIntersection?: ClosestIntersection
-}
 
 function instructionToXYMovement(instruction: string): XY {
   const [direction, value] = [
@@ -18,79 +14,29 @@ function instructionToXYMovement(instruction: string): XY {
   }
 }
 
-function getManhattenDistanceClosestIntersection(
+function getManhattanDistance({ x, y }: XY) {
+  return Math.abs(x) + Math.abs(y)
+}
+
+function getManhattanDistanceClosestIntersection(
   wireAStr: WirePathStr,
   wireBStr: WirePathStr
 ): number {
-  const wireAPasses = new Set<string>()
-  const wirePathA: XY[] = wireAStr.split(",").reduce(
-    (acc, instruction) => {
-      let latestPos = acc[acc.length - 1]
-      const movement = instructionToXYMovement(instruction)
+  const wireA = wireStrToXYPath(wireAStr)
+  const wireB = wireStrToXYPath(wireBStr)
+  const intersections = wireA
+    .flatMap((val, i, arr) => {
+      if (!i) return []
+      return getIntersectionsOnPath([arr[i - 1], val], wireB)
+    })
+    .map((intersection) => ({
+      ...intersection,
+      manhattanDistance: getManhattanDistance(intersection),
+    }))
 
-      while (movement.x || movement.y) {
-        const diffX = movement.x === 0 ? 0 : movement.x > 0 ? 1 : -1
-        const diffY = movement.y === 0 ? 0 : movement.y > 0 ? 1 : -1
-
-        latestPos = {
-          x: latestPos.x + diffX,
-          y: latestPos.y + diffY,
-        }
-
-        acc.push(latestPos)
-
-        wireAPasses.add(latestPos.x + "|" + latestPos.y)
-
-        movement.x += -diffX
-        movement.y += -diffY
-      }
-
-      return acc
-    },
-    [{ x: 0, y: 0 }]
-  )
-
-  const pathBIntersection: XYPosWithPossibleIntersection = wireBStr
-    .split(",")
-    .reduce(
-      (latest, instruction) => {
-        const movement = instructionToXYMovement(instruction)
-
-        while (movement.x || movement.y) {
-          const diffX = movement.x === 0 ? 0 : movement.x > 0 ? 1 : -1
-          const diffY = movement.y === 0 ? 0 : movement.y > 0 ? 1 : -1
-
-          latest = {
-            ...latest,
-            x: latest.x + diffX,
-            y: latest.y + diffY,
-          }
-
-          if (wireAPasses.has(latest.x + "|" + latest.y)) {
-            const distance = Math.abs(latest.x) + Math.abs(latest.y)
-
-            if (
-              !latest.closestIntersection ||
-              distance <= latest.closestIntersection.distance
-            ) {
-              latest.closestIntersection = {
-                x: latest.x,
-                y: latest.y,
-                distance,
-              }
-            }
-          }
-
-          movement.x += -diffX
-          movement.y += -diffY
-        }
-
-        return latest
-      },
-      { x: 0, y: 0 } as XYPosWithPossibleIntersection
-    )
-
-  return pathBIntersection.closestIntersection?.distance || 0
+  return intersections.length
+    ? Math.min(...intersections.map((i) => i.manhattanDistance))
+    : 0
 }
 
 function wireStrToXYPath(wireStr: WirePathStr): XY[] {
@@ -149,8 +95,6 @@ function getIntersectionsOnPath(
 
     if (lineIntersect) {
       if (lineIntersect.x === 0 && lineIntersect.y === 0) return []
-      console.log(`lineIntersect`, lineIntersect)
-
       return [
         {
           ...lineIntersect,
@@ -184,16 +128,16 @@ describe("Crossed Wires", () => {
   describe("What is the Manhattan distance from the central port to the closest intersection?", () => {
     test("Examples", () => {
       expect(
-        getManhattenDistanceClosestIntersection("R8,U5,L5,D3", "U7,R6,D4,L4")
+        getManhattanDistanceClosestIntersection("R8,U5,L5,D3", "U7,R6,D4,L4")
       ).toEqual(6)
       expect(
-        getManhattenDistanceClosestIntersection(
+        getManhattanDistanceClosestIntersection(
           "R75,D30,R83,U83,L12,D49,R71,U7,L72",
           "U62,R66,U55,R34,D71,R55,D58,R83"
         )
       ).toEqual(159)
       expect(
-        getManhattenDistanceClosestIntersection(
+        getManhattanDistanceClosestIntersection(
           "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51",
           "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"
         )
@@ -204,7 +148,7 @@ describe("Crossed Wires", () => {
       const [wireA, wireB] = readFileIntoLines(`${__dirname}/input.txt`)
 
       expect(
-        getManhattenDistanceClosestIntersection(wireA, wireB)
+        getManhattanDistanceClosestIntersection(wireA, wireB)
       ).toMatchInlineSnapshot(`316`)
     })
   })
