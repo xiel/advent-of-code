521900fce43375d3dcc8d91579927161bc7f5191
Felix Leupold <felix@xiel.de>
2020-12-13 12:55:57 +0100

    fix: demo for codesandbox?

--- src/Day 11 - Seating System/src/index.ts
+++ src/Day 11 - Seating System/src/index.ts
@@ -2,29 +2,31 @@
 import seatsFile from "../test/fixtures/input.txt";
 import { gameOfSeatingSystem, SeatMap } from "./GameOfSeatingSystem";
 
-(async () => {
-  const list = await fetch(seatsFile).then((res) => res.text());
-  const splitSeats = list
-    .split("\n")
-    .filter(Boolean)
-    .map((row) => row.split(""));
+fetch(seatsFile)
+  .then((res) => res.text())
+  .then((list) => {
+    const splitSeats = list
+      .split("\n")
+      .filter(Boolean)
+      .map((row) => row.split(""));
 
-  const frames: SeatMap[] = [];
+    const frames: SeatMap[] = [];
 
-  gameOfSeatingSystem(splitSeats, false, (map) => frames.push(map));
-  renderNextFrame();
+    gameOfSeatingSystem(splitSeats, false, (map) => frames.push(map));
+    renderNextFrame();
 
-  function renderNextFrame() {
-    const frame = frames.shift();
+    function renderNextFrame() {
+      const frame = frames.shift();
 
-    if (frame) {
-      requestAnimationFrame(() => {
-        document.body.innerHTML = frame
-          .map((row) => row.join(""))
-          .join("<br/>");
+      if (frame) {
+        requestAnimationFrame(() => {
+          document.body.innerHTML = frame
+            .map((row) => row.join(""))
+            .join("<br/>");
 
-        setTimeout(() => renderNextFrame(), 100);
-      });
+          setTimeout(() => renderNextFrame(), 100);
+        });
+      }
     }
-  }
-})().catch(console.error);
+  })
+  .catch(console.error);
