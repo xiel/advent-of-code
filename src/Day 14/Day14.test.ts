424076c90a35439d5eedd7ea292cdd050e4d19c2
Felix Leupold <felix@xiel.de>
2020-12-13 20:27:32 +0100

    2020 - Day 14 - preparation

---
+++ src/Day 14/Day14.test.ts
@@ -0,0 +1,19 @@
+import { readFileIntoLines } from "../utils/readFile";
+
+describe("Day 14 - ...", () => {
+  describe("Part I - ...", () => {
+    test("Example", () => {
+      const input = readFileIntoLines(`${__dirname}/fixtures/example.txt`);
+      // expect(fn()).toEqual()
+    });
+
+    // test("Input", () => {
+    //   const input = readFileIntoLines(`${__dirname}/fixtures/input.txt`);
+    //   expect(fn()).toMatchInlineSnapshot();
+    // });
+  });
+});
+
+function fn(lines: string[]) {
+  return 0;
+}
