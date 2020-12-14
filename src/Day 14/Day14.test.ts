19b377c7dfeecbb81bcbbe272044acc39a391c70
Felix Leupold <felix@xiel.de>
2020-12-14 08:35:35 +0100

    2020 - Day 14

--- src/Day 14/Day14.test.ts
+++ src/Day 14/Day14.test.ts
@@ -1,19 +1,63 @@
 import { readFileIntoLines } from "../utils/readFile";
 
-describe("Day 14 - ...", () => {
-  describe("Part I - ...", () => {
+describe("Day 14 - Docking Data", () => {
+  describe("Part I - What is the sum of all values left in memory after it completes?", () => {
     test("Example", () => {
       const input = readFileIntoLines(`${__dirname}/fixtures/example.txt`);
-      // expect(fn()).toEqual()
+      expect(
+        applyMask(
+          "000000000000000000000000000000001011",
+          "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X"
+        )
+      ).toEqual("000000000000000000000000000001001001");
+
+      expect(
+        applyMask(
+          "000000000000000000000000000001100101",
+          "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X"
+        )
+      ).toEqual("000000000000000000000000000001100101");
     });
 
-    // test("Input", () => {
-    //   const input = readFileIntoLines(`${__dirname}/fixtures/input.txt`);
-    //   expect(fn()).toMatchInlineSnapshot();
-    // });
+    test("Input", () => {
+      const input = readFileIntoLines(`${__dirname}/fixtures/input.txt`);
+      expect(runDockingProgram(input)).toEqual(9967721333886);
+    });
   });
 });
 
-function fn(lines: string[]) {
-  return 0;
+// mask example: XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
+function applyMask(value: string, mask: string) {
+  const newValue = value.padStart(mask.length, "0").split("");
+  mask.split("").forEach((letter, i) => {
+    if (letter === "1") {
+      newValue[i] = "1";
+    } else if (letter === "0") {
+      newValue[i] = "0";
+    }
+  });
+  return newValue.join("");
+}
+
+function runDockingProgram(instructionsStrs: string[]) {
+  const memory = new Map<number, number>();
+  let currentMask = "";
+
+  for (const instr of instructionsStrs) {
+    const [assignee, valueStr] = instr.split(" = ");
+
+    if (assignee === "mask") {
+      currentMask = valueStr;
+      continue;
+    }
+
+    if (assignee.startsWith("mem[")) {
+      const valueNum = parseInt(valueStr, 10);
+      const address = parseInt(assignee.match(/\d+/)![0], 10);
+      const value = applyMask(valueNum.toString(2), currentMask);
+      memory.set(address, parseInt(value, 2));
+    }
+  }
+
+  return Array.from(memory.values()).reduce((a, b) => a + b, 0);
 }
