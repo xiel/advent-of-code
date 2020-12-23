5e7b7081fda675ab5cba7180f6af518984cb67e2
Felix Leupold <felix@xiel.de>
2020-12-23 14:34:43 +0100

    Day 23 - Part II - remove prev and get min+max in one go

--- src/Day-23-Crab-Cups/Day23.ts
+++ src/Day-23-Crab-Cups/Day23.ts
@@ -1,17 +1,13 @@
 interface Cup {
   value: number;
-  valueStr: string;
   next: Cup;
-  prev: Cup;
 }
 
 export function playCrabCups(startingOrder: string | string[], rounds = 10) {
   const cupStrs =
     typeof startingOrder === "string" ? startingOrder.split("") : startingOrder;
   const cupNums = cupStrs.map((n) => parseInt(n));
-  const cupsMin = min(cupNums);
-  const cupsMax = max(cupNums);
-  const cups = makeCups(cupNums);
+  const { cups, min, max } = makeCups(cupNums);
   let currentCup = cups.get(cupNums[0])!;
 
   let round = 0;
@@ -29,21 +25,22 @@ export function playCrabCups(startingOrder: string | string[], rounds = 10) {
     // They are removed from the circle; cup spacing is adjusted as necessary to maintain the circle.
     const lastPickedUpCup = pickedUpCups[pickedUpCups.length - 1];
     currentCup.next = lastPickedUpCup.next;
-    lastPickedUpCup.next.prev = currentCup;
 
     // The crab selects a destination cup: the cup with a label equal to the current cup's label minus one.
     let destinationCupCandidateValue = currentCup.value - 1;
 
-    // If this would select one of the cups that was just picked up, the crab will keep subtracting one until it finds a cup that wasn't just picked up.
+    // If this would select one of the cups that was just picked up,
+    // the crab will keep subtracting one until it finds a cup that wasn't just picked up.
     while (
       !cups.has(destinationCupCandidateValue) ||
       pickedUpCups.find((c) => c.value === destinationCupCandidateValue)
     ) {
       destinationCupCandidateValue--;
 
-      // If at any point in this process the value goes below the lowest value on any cup's label, it wraps around to the highest value on any cup's label instead.
-      if (destinationCupCandidateValue < cupsMin) {
-        destinationCupCandidateValue = cupsMax;
+      // If at any point in this process the value goes below the lowest value on any cup's label,
+      // it wraps around to the highest value on any cup's label instead.
+      if (destinationCupCandidateValue < min) {
+        destinationCupCandidateValue = max;
       }
     }
 
@@ -57,11 +54,9 @@ export function playCrabCups(startingOrder: string | string[], rounds = 10) {
 
     // connect start of the picked up cups
     destinationCup.next = pickedUpCups[0];
-    pickedUpCups[0].prev = destinationCup;
 
     // connect end of the picked up cups
     pickedUpCups[pickedUpCups.length - 1].next = afterInsertCup;
-    afterInsertCup.prev = pickedUpCups[pickedUpCups.length - 1];
 
     // The crab selects a new current cup: the cup which is immediately clockwise of the current cup.
     currentCup = currentCup.next;
@@ -73,7 +68,7 @@ export function playCrabCups(startingOrder: string | string[], rounds = 10) {
 
   while (current.next !== cupOne) {
     current = current.next;
-    numsAfterOne += current.valueStr;
+    numsAfterOne += current.value;
   }
 
   const productOfNextTwo = cupOne.next.value * cupOne.next.next.value;
@@ -86,31 +81,39 @@ export function playCrabCups(startingOrder: string | string[], rounds = 10) {
 
 function makeCups(cupNums: number[]) {
   const cupsMap = new Map<number, Cup>();
+  let min = Infinity;
+  let max = -Infinity;
+
   let prev: Cup | undefined;
 
   // build chain of cups nodes
   const cups = cupNums.map((value) => {
-    const cup = {
+    const cup: Cup = {
       value,
-      valueStr: value.toString(),
-      prev,
-    } as Cup;
+      next: null as never,
+    };
 
     if (prev) {
       prev.next = cup;
     }
 
     prev = cup;
+    cupsMap.set(cup.value, cup);
+
+    if (value > max) {
+      max = value;
+    }
+    if (value < min) {
+      min = value;
+    }
+
     return cup;
   });
 
-  // connect first and last
+  // connect last to first
   cups[cups.length - 1].next = cups[0];
-  cups[0].prev = cups[cups.length - 1];
 
-  cups.forEach((cup) => cupsMap.set(cup.value, cup));
-
-  return cupsMap;
+  return { cups: cupsMap, min, max };
 }
 
 export function prepareOneMillionCups(startingOrder: string) {
@@ -124,24 +127,4 @@ export function prepareOneMillionCups(startingOrder: string) {
   }
 
   return cups;
-}
-
-function min(nums: number[]) {
-  let currMin = Infinity;
-  for (const num of nums) {
-    if (num < currMin) {
-      currMin = num;
-    }
-  }
-  return currMin;
-}
-
-function max(nums: number[]) {
-  let currMax = -Infinity;
-  for (const num of nums) {
-    if (num > currMax) {
-      currMax = num;
-    }
-  }
-  return currMax;
 }
