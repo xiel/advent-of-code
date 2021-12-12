8090900bc5fe53c653d5d1648390b99f4e03f964
Felix Leupold <felix@xiel.de>
2021-12-12 11:15:52 +0100

    day 12 - part 2 - clean up

--- src/2021/Day12/Day12.test.ts
+++ src/2021/Day12/Day12.test.ts
@@ -33,8 +33,8 @@ describe("Day 12: Passage Pathing", () => {
 
 type Cave = string;
 
-function countPathsInCaveSystem(connections: string[]) {
-  const caveMap = connections.reduce((map, connection) => {
+function buildCaveMap(connections: string[]) {
+  return connections.reduce((map, connection) => {
     const [from, to] = connection.split("-");
 
     if (!map.has(from)) map.set(from, new Set<Cave>());
@@ -45,15 +45,17 @@ function countPathsInCaveSystem(connections: string[]) {
 
     return map;
   }, new Map<Cave, Set<Cave>>());
+}
 
-  // How many paths through this cave system are there that visit small caves at most once?
+function countPathsInCaveSystem(connections: string[]) {
+  const caveMap = buildCaveMap(connections);
   return findPaths("start").length;
 
   function findPaths(
     cave: Cave,
     visited: Set<Cave> = new Set<Cave>(),
     path: Cave[] = []
-  ): string[][] {
+  ): Cave[][] {
     const currentPath = [...path, cave];
 
     if (cave === "end") {
@@ -73,31 +75,11 @@ function countPathsInCaveSystem(connections: string[]) {
       return findPaths(nextCave, newVisited, currentPath);
     });
   }
-
-  function isBigCave(cave: Cave) {
-    return cave[0].toUpperCase() === cave[0];
-  }
 }
 
-/**
- * After reviewing the available paths, you realize you might have time to visit a single small cave twice.
- * Specifically, big caves can be visited any number of times
- * However, the caves named start and end can only be visited exactly once each: once you leave the start cave, you may not return to it, and once you reach the end cave, the path must end immediately.
- */
 function countPathsInCaveSystemTwo(connections: string[]) {
-  const caveMap = connections.reduce((map, connection) => {
-    const [from, to] = connection.split("-");
+  const caveMap = buildCaveMap(connections);
 
-    if (!map.has(from)) map.set(from, new Set<Cave>());
-    if (!map.has(to)) map.set(to, new Set<Cave>());
-
-    map.get(from)!.add(to);
-    map.get(to)!.add(from);
-
-    return map;
-  }, new Map<Cave, Set<Cave>>());
-
-  // How many paths through this cave system are there that visit small caves at most once?
   return findPaths("start").length;
 
   function findPaths(
@@ -105,7 +87,7 @@ function countPathsInCaveSystemTwo(connections: string[]) {
     visited: Set<Cave> = new Set<Cave>(),
     path: Cave[] = [],
     smallCaveVisitedTwice: Cave = ""
-  ): string[][] {
+  ): Cave[][] {
     const currentPath = [...path, cave];
 
     if (cave === "end") {
@@ -117,32 +99,24 @@ function countPathsInCaveSystemTwo(connections: string[]) {
     newVisited.add(cave);
 
     return Array.from(nextCaves).flatMap((nextCave) => {
-      if (isBigCave(nextCave)) {
+      if (isBigCave(nextCave) || !newVisited.has(nextCave)) {
         return findPaths(
           nextCave,
           newVisited,
           currentPath,
           smallCaveVisitedTwice
         );
-      }
 
-      // a single small cave can be visited at most twice, and the remaining small caves can be visited at most once.
-      if (!newVisited.has(nextCave)) {
-        return findPaths(
-          nextCave,
-          newVisited,
-          currentPath,
-          smallCaveVisitedTwice
-        );
-      } else if (nextCave !== "start" && !smallCaveVisitedTwice) {
+        // A single small cave can be visited at most twice, and the remaining small caves can be visited at most once.
+      } else if (!smallCaveVisitedTwice && nextCave !== "start") {
         return findPaths(nextCave, newVisited, currentPath, cave);
+      } else {
+        return [];
       }
-
-      return [];
     });
   }
+}
 
-  function isBigCave(cave: Cave) {
-    return cave[0].toUpperCase() === cave[0];
-  }
+function isBigCave(cave: Cave) {
+  return cave[0].toUpperCase() === cave[0];
 }
