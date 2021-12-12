b5fd4201e32d702013d63854317bd5e8c6cfa91e
Felix Leupold <felix@xiel.de>
2021-12-12 10:27:37 +0100

    day 12 - part 1

---
+++ src/2021/Day12/Day12.test.ts
@@ -0,0 +1,70 @@
+import { readFileIntoLines } from "../../utils/readFile";
+
+const example = `start-A
+start-b
+A-c
+A-b
+b-d
+A-end
+b-end`;
+
+// https://adventofcode.com/2021/day/12
+describe("Day 12: Passage Pathing", () => {
+  test("Example", () => {
+    const input = example.split(/\n/);
+    expect(countPathsInCaveSystem(input)).toBe(10);
+  });
+
+  test("01", () => {
+    const input = readFileIntoLines(`${__dirname}/input.txt`);
+    expect(countPathsInCaveSystem(input)).toBe(4411);
+  });
+});
+
+type Cave = string;
+
+function countPathsInCaveSystem(connections: string[]) {
+  const caveMap = connections.reduce((map, connection) => {
+    const [from, to] = connection.split("-");
+
+    if (!map.has(from)) map.set(from, new Set<Cave>());
+    if (!map.has(to)) map.set(to, new Set<Cave>());
+
+    map.get(from)!.add(to);
+    map.get(to)!.add(from);
+
+    return map;
+  }, new Map<Cave, Set<Cave>>());
+
+  // How many paths through this cave system are there that visit small caves at most once?
+  return findPaths("start").length;
+
+  function findPaths(
+    cave: Cave,
+    visited: Set<Cave> = new Set<Cave>(),
+    path: Cave[] = []
+  ): string[][] {
+    const currentPath = [...path, cave];
+
+    if (cave === "end") {
+      return [currentPath];
+    }
+
+    const nextCaves = caveMap.get(cave)!;
+    const newVisited = new Set(visited);
+    newVisited.add(cave);
+
+    return Array.from(nextCaves).flatMap((nextCave) => {
+      // big caves (written in uppercase, like A) and small caves (written in lowercase, like b)
+      // don't visit small caves more than once
+      if (newVisited.has(nextCave) && !isBigCave(nextCave)) {
+        return [];
+      }
+      return findPaths(nextCave, newVisited, currentPath);
+    });
+  }
+
+  function isBigCave(cave: Cave) {
+    return cave[0].toUpperCase() === cave[0];
+  }
+}
