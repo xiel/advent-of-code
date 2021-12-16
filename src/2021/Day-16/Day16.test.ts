4860f6cef91ecf34a1820fa0cd684e2985acbebf
Felix Leupold <felix@xiel.de>
2021-12-16 12:04:53 +0100

    day 16 - part 2

--- src/2021/Day-16/Day16.test.ts
+++ src/2021/Day-16/Day16.test.ts
@@ -1,5 +1,6 @@
-import { readExampleIntoLines, readFileIntoLines } from "../../utils/readFile";
+import { readFileIntoLines } from "../../utils/readFile";
 
+// https://adventofcode.com/2021/day/16
 describe("Day 16: Packet Decoder", () => {
   const input = readFileIntoLines(__dirname + "/input.txt");
 
@@ -11,12 +12,13 @@ describe("Day 16: Packet Decoder", () => {
     expect(solve("620080001611562C8802118E34").versionSum).toBe(12);
     expect(solve("C0015000016115A2E0802F182340").versionSum).toBe(23);
     expect(solve("A0016C880162017C3686B18A3D4780").versionSum).toBe(31);
-
     expect(solve(input[0]).versionSum).toBe(989);
   });
 
-  test("Part 02 - ..", () => {
-    // ...
+  test("Part 02 - What do you get if you evaluate the expression represented by your hexadecimal-encoded BITS transmission?", () => {
+    expect(solve("C200B40A82").value).toBe(3);
+    expect(solve("04005AC33890").value).toBe(54);
+    expect(solve(input[0]).value).toBe(7936430475134);
   });
 });
 
@@ -38,16 +40,23 @@ const hexBin = {
   E: "1110",
   F: "1111",
 };
+const { min, max } = Math;
 
 type Hex = keyof typeof hexBin;
+
+interface Package {
+  version: number;
+  typeId: number;
+  endIndex: number; // not included in package
+  decimalValue?: number;
+  value?: number;
+  subPackages: Package[];
+}
 
 function solve(line: string) {
   const chars = line.split("");
   const bin = chars.map((c) => hexBin[c as Hex]);
   const binaryFull = bin.join("").split("").join("");
-
-  console.log("!!!             !!!!!!!!!!!!!      !!!!!!!1");
-  console.log(`binary`, line, binaryFull);
 
   const packageSet = new Set<Package>();
   const outermostPackage = parsePackage(0);
@@ -57,80 +66,52 @@ function solve(line: string) {
     .map((p) => p.version)
     .reduce((a, b) => a + b, 0);
 
-  console.log({
-    versionSum,
-    outermostPackage,
-  });
-
   return {
     versionSum,
+    // Part 2
+    get value() {
+      return outermostPackage.value;
+    },
   };
 
-  interface Package {
-    version: number;
-    typeId: number;
-    endIndex: number; // not included
-    // one or the other
-    decimalValue?: number;
-    subPackages: Package[];
-  }
-
-  // The BITS transmission contains a single packet at its outermost layer
-  // which itself contains many other packets.
-  // ----
-  // The hexadecimal representation of this packet might encode a few extra 0 bits at the end; these are not part of the transmission and should be ignored.
-  function parsePackage(
-    startingAt: number,
-    binary = binaryFull
-    // until?: { packageNumber: number } | { bla: number }
-  ): Package {
+  function parsePackage(startingAt: number, binary = binaryFull): Package {
     // Every packet begins with a standard header:
     // - the FIRST THREE BITS encode the packet VERSION,
     // - the NEXT THREE BITs encode the packet TYPE ID.
-    // ...
     const [version, typeId] = [
       binary.slice(startingAt, startingAt + 3),
       binary.slice(startingAt + 3, startingAt + 6),
     ].map(toDec);
 
     const subPackages: Package[] = [];
+
     let cursor = startingAt + 6;
     let decimalValue: number | undefined = undefined;
 
-    console.log("parsing pack, header:", { version, typeId, cursor });
-
     if (typeId === 4) {
-      // Packets with type ID 4 represent a LITERAL VALUE. -> encode a single binary number
-      // To do this, the binary number is padded with leading zeroes until its LENGTH IS A MULTIPLE OF FOUR BITS,
-      //  and then it is broken into GROUPS OF FOUR BITS.
-      // Each group is prefixed by a 1 bit except the last group, which is prefixed by a 0 bit.
-      // These GROUPS OF FIVE BITS immediately follow the packet header.
+      // Packets with type ID 4 represent a LITERAL VALUE -> encode a single binary number
       let hasEnded = false;
       let literalValueBits = "";
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      const groupOfFive = [...binary.slice(cursor).matchAll(/.{5}/g)].flatMap(
-        ([group]) => {
-          if (hasEnded) return [];
-          if (group.startsWith("0")) hasEnded = true;
-          const valueBits = group.slice(1);
-          literalValueBits += valueBits;
-          cursor += group.length;
-          return [group];
-        }
-      );
+
+      [...binary.slice(cursor).matchAll(/.{5}/g)].flatMap(([group]) => {
+        if (hasEnded) return [];
+        if (group.startsWith("0")) hasEnded = true;
+        const valueBits = group.slice(1);
+        literalValueBits += valueBits;
+        cursor += group.length;
+        return [group];
+      });
+
       decimalValue = toDec(literalValueBits);
-      console.log(`decimalValue`, decimalValue);
     } else {
-      // other TYPE IDs are operator that performs some calculation on one or more sub-packets contained within.
+      // Other TYPE IDs are operator that performs some calculation on one or more sub-packets contained within.
       // An OPERATOR packet contains one or more packets
       // the bit immediately after the packet header is called the LENGTH TYPE ID:
       const lengthTypeId = binary[cursor];
       cursor += 1;
 
       if (lengthTypeId === "0") {
-        /// If the length type ID is 0,
-        //    then the NEXT 15 BITS are a number that represents the TOTAL LENGTH IN BITS OF THE SUB-PACKETS contained by this packet.
-        const numberBitsInSubPackets = toDec(binary.slice(cursor, cursor + 15)); // end index not included ;
+        const numberBitsInSubPackets = toDec(binary.slice(cursor, cursor + 15));
         cursor += 15;
 
         let bitsRemaining = numberBitsInSubPackets;
@@ -138,22 +119,17 @@ function solve(line: string) {
           cursor,
           cursor + numberBitsInSubPackets
         );
-
         cursor += numberBitsInSubPackets;
-        // After reading 11 and 16 bits of sub-packet data, the total length indicated in L (27) is reached, and so parsing of this packet stops.
+
         while (bitsRemaining > 0) {
           const startSubAt = numberBitsInSubPackets - bitsRemaining;
           const subPacket = parsePackage(startSubAt, subPaketBinary);
           subPackages.push(subPacket);
-          bitsRemaining -= subPacket.endIndex - startSubAt; // TODO this should not be negative i gues
+          bitsRemaining -= subPacket.endIndex - startSubAt;
         }
       } else if (lengthTypeId) {
-        // If the length type ID is 1,
-        //  then the NEXT 11 BITS are a number that represents the NUMBER OF SUB-PACKETS IMMEDIATELY CONTAINED BY THIS PACKET.
-        // After reading 3 complete sub-packets, the number of sub-packets indicated in L (3) is reached, and so parsing of this packet stops.
         const numberOfSubpackages = toDec(binary.slice(cursor, cursor + 11)); // end index not included
         cursor += 11;
-        console.log(`numberOfSubpackages !!`, numberOfSubpackages);
 
         while (subPackages.length < numberOfSubpackages) {
           const startSubAt = cursor;
@@ -169,6 +145,33 @@ function solve(line: string) {
       typeId,
       subPackages,
       decimalValue,
+      get value(): number {
+        const subValues = subPackages.map((p) => p.decimalValue ?? p.value!);
+        switch (typeId) {
+          case 0:
+            return subValues.reduce((a, b) => a + b, 0);
+          case 1:
+            return subValues.reduce((a, b) => a * b, 1);
+          case 2:
+            return min(...subValues);
+          case 3:
+            return max(...subValues);
+          case 5: {
+            const [first, second] = subValues;
+            return first > second ? 1 : 0;
+          }
+          case 6: {
+            const [first, second] = subValues;
+            return first < second ? 1 : 0;
+          }
+          case 7: {
+            const [first, second] = subValues;
+            return first === second ? 1 : 0;
+          }
+          default:
+            throw Error("unknown value " + typeId);
+        }
+      },
       endIndex: cursor,
     };
     packageSet.add(parsedPackage);
