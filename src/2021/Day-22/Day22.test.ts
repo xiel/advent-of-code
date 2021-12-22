1fcafa02ff18acaccf151518f3da43dddfcc78e6
Felix Leupold <felix@xiel.de>
2021-12-22 08:14:49 +0100

    day 22 - add examples

--- src/2021/Day-22/Day22.test.ts
+++ src/2021/Day-22/Day22.test.ts
@@ -2,31 +2,8 @@ import { readExampleIntoLines, readFileIntoLines } from "../../utils/readFile";
 
 // https://adventofcode.com/2021/day/22
 describe("Day 22: Reactor Reboot", () => {
-  const example = readExampleIntoLines(`
-    on x=-20..26,y=-36..17,z=-47..7
-    on x=-20..33,y=-21..23,z=-26..28
-    on x=-22..28,y=-29..23,z=-38..16
-    on x=-46..7,y=-6..46,z=-50..-1
-    on x=-49..1,y=-3..46,z=-24..28
-    on x=2..47,y=-22..22,z=-23..27
-    on x=-27..23,y=-28..26,z=-21..29
-    on x=-39..5,y=-6..47,z=-3..44
-    on x=-30..21,y=-8..43,z=-13..34
-    on x=-22..26,y=-27..20,z=-29..19
-    off x=-48..-32,y=26..41,z=-47..-37
-    on x=-12..35,y=6..50,z=-50..-2
-    off x=-48..-32,y=-32..-16,z=-15..-5
-    on x=-18..26,y=-33..15,z=-7..46
-    off x=-40..-22,y=-38..-28,z=23..41
-    on x=-16..35,y=-41..10,z=-47..6
-    off x=-32..-23,y=11..30,z=-14..3
-    on x=-49..-5,y=-3..45,z=-29..18
-    off x=18..30,y=-20..-8,z=-3..13
-    on x=-41..9,y=-7..43,z=-33..15
-    on x=-54112..-39298,y=-85059..-49293,z=-27449..7877
-    on x=967..23432,y=45373..81175,z=27513..53682
-  `);
-
+  const example = readFileIntoLines(__dirname + "/example.txt");
+  const exampleBig = readFileIntoLines(__dirname + "/exampleBig.txt");
   const input = readFileIntoLines(__dirname + "/input.txt");
 
   test("Part 01 - ...", () => {
@@ -45,17 +22,16 @@ describe("Day 22: Reactor Reboot", () => {
   });
 
   test("Part 02 - ..", () => {
-    // ...
+    expect(solve2(example).cellsOnAfterSteps).toBe(2758514936282235);
+    expect(solve2(input).cellsOnAfterSteps).toBe(-1);
   });
 });
 
 const { min, max, abs, floor, ceil, round } = Math;
-type XYZ = [number, number, number];
 const _ = (...args: unknown[]) => args.join();
 
-function solve(lines: string[]) {
-  const map = new Map<string, number>();
-  const steps = lines.map((l) => {
+function getSteps(lines: string[]) {
+  return lines.map((l) => {
     const [turn] = l.split(" ");
     const matches = [...l.matchAll(/-?\d+/g)].map((n) => +n[0]);
     const [fromX, toX, fromY, toY, fromZ, toZ] = matches;
@@ -69,6 +45,11 @@ function solve(lines: string[]) {
       toZ,
     };
   });
+}
+
+function solve(lines: string[]) {
+  const map = new Map<string, number>();
+  const steps = getSteps(lines);
 
   // TODO: can i do the map virstual somehow? execute the steps on read for a cell?
   // start from last step and find the latest step that sets the value
@@ -77,11 +58,9 @@ function solve(lines: string[]) {
     fromX = max(fromX, -50);
     fromY = max(fromY, -50);
     fromZ = max(fromZ, -50);
-
     toX = min(toX, 50);
     toY = min(toY, 50);
     toZ = min(toZ, 50);
-
     for (let x = fromX; x <= toX; x++) {
       for (let y = fromY; y <= toY; y++) {
         for (let z = fromZ; z <= toZ; z++) {
@@ -104,5 +83,11 @@ function solve(lines: string[]) {
     get cellsOnAfterSteps() {
       return [...map.values()].reduce((a, b) => a + b, 0);
     },
+  };
+}
+
+function solve2() {
+  return {
+    cellsOnAfterSteps: 1,
   };
 }
