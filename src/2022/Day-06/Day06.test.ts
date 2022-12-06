2b14c5e5e5500396b193cccc0d5c934ceb7caf8c
Felix Leupold <felix@xiel.de>
2022-12-06 10:35:35 +0100

    day 6 - part 1 & 2 - cleanup & speed

--- src/2022/Day-06/Day06.test.ts
+++ src/2022/Day-06/Day06.test.ts
@@ -3,36 +3,25 @@ import { readFileIntoLines } from "../../utils/readFile";
 // https://adventofcode.com/2022/day/6
 describe("Day 06", () => {
   test("Example", () => {
-    expect(part1("mjqjpqmgbljsphdztnvjfqwrcgsmlb")).toEqual(7);
-    expect(part1("bvwbjplbgvbhsrlpgdmjqwftvncz")).toEqual(5);
-    expect(part2("mjqjpqmgbljsphdztnvjfqwrcgsmlb")).toEqual(19);
-    expect(part2("bvwbjplbgvbhsrlpgdmjqwftvncz")).toEqual(23);
+    expect(solve("mjqjpqmgbljsphdztnvjfqwrcgsmlb")).toEqual(7);
+    expect(solve("bvwbjplbgvbhsrlpgdmjqwftvncz")).toEqual(5);
+    expect(solve("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 14)).toEqual(19);
+    expect(solve("bvwbjplbgvbhsrlpgdmjqwftvncz", 14)).toEqual(23);
   });
 
   test("Part 1 & 2", () => {
-    expect(part1(readFileIntoLines(__dirname + "/input.txt")[0])).toEqual(1929);
-    expect(part2(readFileIntoLines(__dirname + "/input.txt")[0])).toEqual(3298);
+    expect(solve(readFileIntoLines(__dirname + "/input.txt")[0])).toEqual(1929);
+    expect(solve(readFileIntoLines(__dirname + "/input.txt")[0], 14)).toEqual(
+      3298
+    );
   });
 });
 
-function part1(str: string) {
+function solve(str: string, size = 4) {
   for (let i = 0; i < str.length; i++) {
-    const set = new Set(
-      [i, i - 1, i - 2, i - 3].map((j) => str[j]).filter(Boolean)
-    );
-    if (set.size === 4) {
-      return i + 1;
-    }
-  }
-}
-function part2(str: string) {
-  for (let i = 0; i < str.length; i++) {
-    const set = new Set(
-      Array.from({ length: 14 }, (_, j) => i - j)
-        .map((j) => str[j])
-        .filter(Boolean)
-    );
-    if (set.size === 14) {
+    const set = new Set(Array.from(str.substring(i - size + 1, i + 1)));
+
+    if (set.size === size) {
       return i + 1;
     }
   }
